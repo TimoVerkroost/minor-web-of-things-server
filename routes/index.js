0cc6167a5c6a3c7b3b91406e9dae45bb63095dff
Timo Verkroost <timo.verkroost@gmail.com>
2017-04-13 11:05:10 +0200

    config added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+require('dotenv').config();
+
+var server_url = process.env.SERVER_URL;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var api_url = "/api.php?t=sdc&d=T111&td=T222&c=ff0000";
+  var callURL = server_url + API_KEY + searchURL;
+  request(callURL, function (error, response, data) {
+    res.locals.data =  JSON.parse(data);
+    res.render('index', { title: 'Express' });
+  });
 });
 
 module.exports = router;
